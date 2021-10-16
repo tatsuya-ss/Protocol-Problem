41ae38181466366eac525d8f76ec0e0990ab7504
Reon0429-cat <oonishireon110@gmail.com>
2021-10-17 00:08:29 +0900

    first commit

---
+++ Protocol-ProblemTests/Protocol_ProblemTests.swift
@@ -0,0 +1,33 @@
+//
+//  Protocol_ProblemTests.swift
+//  Protocol-ProblemTests
+//
+//  Created by 大西玲音 on 2021/10/17.
+//
+
+import XCTest
+@testable import Protocol_Problem
+
+class Protocol_ProblemTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Protocol-ProblemUITests/Protocol_ProblemUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Protocol_ProblemUITestsLaunchTests.swift
+//  Protocol-ProblemUITests
+//
+//  Created by 大西玲音 on 2021/10/17.
+//
+
+import XCTest
+
+class Protocol_ProblemUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
